0b081bf8f6bc5959e8ebc2fbbd43b1ba245dd5f8
tamikaivi <yukisaki1998@gmail.com>
2021-05-09 20:19:36 -0400

    fix send message

--- src/app/modules/mainPage/components/tab3/tab3.component.ts
+++ src/app/modules/mainPage/components/tab3/tab3.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
@@ -22,7 +22,7 @@ export class Tab3Component implements OnInit {
   postularForm: FormGroup;
   mensaje = 'Hay una nueva postulación a su pasantía';
 
-  constructor(private http: HttpClient,private route: ActivatedRoute) {
+  constructor(private http: HttpClient,private route: ActivatedRoute,private router: Router) {
     this.correo = '';
     this.emailForm=new FormGroup({
       to_name: new FormControl('', [Validators.required]),
@@ -40,42 +40,19 @@ export class Tab3Component implements OnInit {
     console.log("tab3", this.emailForm.value)
   }
 
-  public sendEmail(e: Event) {
-
-    console.log("EVENTO",e.preventDefault());
-    e.preventDefault();
-    emailjs.sendForm('service_5n3w2me', 'template_p2qmxmr', e.target as HTMLFormElement, 'user_h3wl5X7fbp7lUayv6EeT6')
-      .then((result: EmailJSResponseStatus) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-
-      let data = {
-        service_id: 'service_5n3w2me',
-        template_id: 'template_p2qmxmr',
-        user_id: 'user_h3wl5X7fbp7lUayv6EeT6',
-        template_params: {
-          to_name: this.emailForm.value.to_name,
-          email_to: this.emailForm.value.email_to,
-          phone: this.emailForm.value.phone,
-          message: this.emailForm.value.message
-        }
-      };
-
-      this.http.post('https://api.emailjs.com/api/v1.0/email/send', data, { responseType: 'text' })
-        .subscribe((result) => {
-          alert('Your message has been sent!');
-        }, (error: HttpErrorResponse) => {
-          alert('Oops... ' + error.message);
-        }
-      );
-
-
-  }
   ionViewDidLeave() {
     this.nombre = '';
     this.telefono = null;
   }
 
+  public sendEmail2(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('service_5n3w2me', 'template_xavbtyi', e.target as HTMLFormElement, 'user_h3wl5X7fbp7lUayv6EeT6')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        this.router.navigate(['/mainPage']);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
 }
