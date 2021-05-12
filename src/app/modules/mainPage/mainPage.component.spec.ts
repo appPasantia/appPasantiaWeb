c4ec7a6e3ebc3308cfdb3c747ff2cdb67bf90991
jurgenru <jurgenruegnberg@gmail.com>
2021-05-11 21:25:54 -0400

    ntp

--- src/app/modules/mainPage/mainPage.component.spec.ts
+++ src/app/modules/mainPage/mainPage.component.spec.ts
@@ -9,5 +9,26 @@ export const AngularFireAuthMock = jasmine.createSpy('onLoginGoogle')
   .and.returnValue(Promise.resolve({ uid: 'fakeuser' }));
 
 describe('MainPageComponent', () => {
+    // let component: MainPageComponent;
+    // let fixture: ComponentFixture<MainPageComponent>;
 
+    // beforeEach(async(() => {
+    //     TestBed.configureTestingModule({
+    //         imports:[RouterTestingModule],providers: [
+    //             { provide: AngularFireAuth, useValue: AngularFireAuthMock }
+    //           ],
+    //         declarations: [MainPageComponent]
+    //     })
+    //         .compileComponents();
+    // }));
+
+    // beforeEach(() => {
+    //     fixture = TestBed.createComponent(MainPageComponent);
+    //     component = fixture.componentInstance;
+    //     fixture.detectChanges();
+    // });
+
+    // it('should create', () => {
+    //     expect(component).toBeTruthy();
+    // });
 });
